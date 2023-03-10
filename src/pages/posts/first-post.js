99469dd868c25db49e4865dc1eeb5057453fb40a
Houssin mhamdi <houssinoss010203@gmail.com>
2023-03-10 23:48:41 +0100

    add clsx

--- src/pages/posts/first-post.js
+++ src/pages/posts/first-post.js
@@ -1,7 +1,7 @@
+import Alert from '@/components/Alert';
 import Layout from '@/components/Layout';
 import Head from 'next/head';
 import Link from 'next/link';
-
 export default function firstPost() {
   return (
     <>
@@ -10,9 +10,12 @@ export default function firstPost() {
           <title>First Post</title>
         </Head>
         <h1>First Post</h1>
-        <h2>
-          <Link href="/">← Back to home</Link>
-        </h2>
+        <Alert>
+          <h2>
+            <Link href="/">← Back to home</Link>
+          </h2>
+        </Alert>
+
       </Layout>
     </>
   )
